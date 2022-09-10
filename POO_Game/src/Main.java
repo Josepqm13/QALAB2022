b8a656b899e1060d7a66b7c802714f644a7310cb
Josepqm13 <110746186+Josepqm13@users.noreply.github.com>
2022-09-10 12:46:42 -0500

    Update Main.java

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Editado por JOSEP DANIEL QUINTANILLA MARCA
 
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
